161a9c8bf4db478bb3d776bbdc99d6f4ce4215af
Sham13namo <104632477+Sham13namo@users.noreply.github.com>
2022-05-02 15:06:41 +0700

    commit

--- filter.py
+++ filter.py
@@ -11,12 +11,13 @@ TIME_NEW = 0
 dt = 0.
 def read_sensor():
     data = Serial.readline().decode("ütf-8")[:-2].split(";")
-    
+    return data
+
 
 def S():
     a = []
     for i in range(100):
-        a.append(read_sensor())
+        a.append(read_sensor()[2])
     f.R = np.std(a)
     print("Эта фигня найдена")
 def start():
@@ -30,17 +31,13 @@ f.H = np.array([[1.,0.]])
 f.P *= 1000.
 S()
 f.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
-s = []
-raw  = []
 
-for i in range(120):
-    z = read_sensor()
-    raw.append(z)
+while True:
+    data = read_sensor()
+    print(data)
     f.predict()
-    f.update(z)
+    f.update(data[2])
     x = []
     x.append(f.x[0])
     x.append(f.x[1])
-    s.append(x[0])
-print(raw)
-print(s)
+
