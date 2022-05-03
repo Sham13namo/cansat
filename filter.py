b52b637d2b4c8f15ca51a8725120ee5a7bfc1951
Sham13namo <104632477+Sham13namo@users.noreply.github.com>
2022-05-03 13:25:23 +0700

    commit

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 
+from tkinter import W
 import numpy as np
-import matplotlib as plt
+
 from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import *
 import serial 
@@ -10,7 +11,9 @@ TIME_OLD = 0
 TIME_NEW = 0
 dt = 0.
 def read_sensor():
+    TIME_OLD = TIME_NEW
     data = Serial.readline().decode("ütf-8")[:-2].split(";")
+    TIME_NEW = data[1]
     return data
 
 
@@ -26,18 +29,23 @@ def start():
 start()
 f = KalmanFilter (dim_x=2, dim_z=1)
 f.x = np.array([0., 0.])
-f.P = np.array([[1000.,dt ], [0., 1000.]])
+
 f.H = np.array([[1.,0.]])
 f.P *= 1000.
 S()
 f.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
 
 while True:
+    
+    file = open("log.txt", "w")
     data = read_sensor()
+    dt = TIME_NEW-TIME_OLD
+    f.P = np.array([[1000.,dt ], [0., 1000.]])
     print(data)
     f.predict()
     f.update(data[2])
     x = []
     x.append(f.x[0])
     x.append(f.x[1])
-
+    file.write(x)
+    file.close()
--- horizon.py
+++ horizon.py
@@ -13,12 +13,13 @@ def rot_center(image, rect, angle):
         rot_image = pygame.transform.rotate(image, angle)
         rot_rect = rot_image.get_rect(center=rect.center)
         return rot_image,rot_rect
-angle = -10
+angle = 0
 
 image = pygame.image.load("2.jpeg")
 rect = image.get_rect(center=(400, 300)) 
-surf, r = rot_center(image, rect, angle)
-SCREEN.blit(surf, r)
-pygame.display.flip()
+
 while True:
-    print("Fuck You!")
+    angle+=1
+    surf, r = rot_center(image, rect, angle)
+    SCREEN.blit(surf, r)
+    pygame.display.flip()
